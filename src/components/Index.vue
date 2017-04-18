570bf136a6339dbca8325fc32d0b4a3832218e57
Roy <rxlisbest@gmail.com>
2017-04-18 10:55:26 +0800

    项目初始化

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index" v-text="msg">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'RuiXinglong is a handsome man!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
